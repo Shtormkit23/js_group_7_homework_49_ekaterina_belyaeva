ffb5a7526f9412cbf9050c7084234ed48641c6ad
Belyaeva Ekaterina <katrinamalinashtorm@gmail.com>
2020-07-09 01:14:02 +0600

    Вывод шахматной доски в консоль

--- board.js
+++ board.js
@@ -1,2 +1,23 @@
-console.log('  ██  ██  ██  ██' + '\n' + '██  ██  ██  ██  '+ '\n' + '  ██  ██  ██  ██' + '\n' + '██  ██  ██  ██  ');
-console.log('  ██  ██  ██  ██' + '\n' + '██  ██  ██  ██  '+ '\n' + '  ██  ██  ██  ██' + '\n' + '██  ██  ██  ██  ');
+
+let chess = function(size){
+
+    let chessBlock = '';
+
+    let white = '██';
+    let black = '  ';
+
+    for (let y = 0; y < size; y++) {
+        for (let x = 0; x < size; x++) {
+            if ((x + y) % 2 === 0)
+                chessBlock += black;
+            else
+                chessBlock += white;
+        }
+        chessBlock += "\n";
+    }
+
+    return chessBlock;
+}
+
+chess(8)
+console.log(chess(8))
